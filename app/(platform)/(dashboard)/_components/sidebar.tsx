21bf124060cfd576e98d4989f4b655ae684e53e9
msitabheer20 <msitabheerdey@gmail.com>
2025-04-28 10:44:39 +0530

    add sidebar items

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -10,6 +10,8 @@ import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Accordion } from "@/components/ui/accordion";
 
+import { NavItem, Organization } from "./nav-item";
+
 interface SidebarProps {
   storageKey?: string;
 }
@@ -40,12 +42,58 @@ export const Sidebar = ({
     }
 
     return acc;
-  }, [])
+  }, []);
+
+  const onExpand = (id: string) => {
+    setExpanded((curr) => ({
+      ...curr,
+      [id] : !expanded[id],
+    }));
+  };
+
+  if(!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+    return (
+      <>
+        <Skeleton />
+      </>
+    )
+  }
 
   return (
-    <div>
-      Sidebar
+    <>
+    <div className="font-medium text-xs flex items-center mb-1">
+      <span className="pl-4">
+        Workspaces
+      </span>
+      <Button
+        asChild
+        type="button"
+        size="icon"
+        variant="ghost"
+        className="ml-auto"
+      >
+        <Link href="/select-org">
+          <Plus className="h-4 w-4" />
+        </Link>
+      </Button>
     </div>
+
+    <Accordion
+      type="multiple"
+      defaultValue={defaultAccordionValue}
+      className="space-y-2"
+    >
+      {userMemberships.data.map(({ organization }) => (
+        <NavItem
+          key={organization.id} 
+          isActive={activeOrganization?.id === organization.id}
+          isExpanded={expanded[organization.id]}
+          organization={organization as Organization}
+          onExpand={onExpand}
+        />
+      ))}
+    </Accordion>
+    </>
   )
 }
 
