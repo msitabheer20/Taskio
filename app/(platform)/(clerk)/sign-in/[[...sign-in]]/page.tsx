2936e9ab8989617e146b677c075294c13297579e
msitabheer20 <msitabheerdey@gmail.com>
2025-05-13 10:21:16 +0530

    modify signin page

--- app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.tsx
+++ app/(platform)/(clerk)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,42 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />
+  return <SignIn
+    appearance={{
+      elements: {
+        card: {
+          boxShadow: "none",
+          border: "none",
+          borderRadius: "0.75rem"
+        },
+        cardBox: {
+          boxShadow: "none",
+          overflow: "hidden",
+          border: "1px solid #e5e7eb"
+        },
+        ".cl-internal-1dauvpw": {
+          display: "none !important"
+        },
+        ".cl-internal-piyvrh": {
+          background: "white"
+        },
+        headerTitle: {
+          fontSize: "1.875rem",
+          lineHeight: "2.25rem",
+          textAlign: "center",
+          color: "#111827"
+        },
+        formButtonPrimary: {
+          backgroundColor: "#2563eb",
+          color: "white",
+          "&:hover": {
+            backgroundColor: "#1d4ed8"
+          },
+        },
+        footer: {
+          background: "white"
+        }
+      }
+    }}
+  />
 }
--- config/site.ts
+++ config/site.ts
@@ -1,4 +1,4 @@
 export const siteConfig = {
-  name: "Kanban",
-  description: "Collaborate, manage projects, and reach new productivity peaks.",
+  name: "Taskio",
+  description: "Collaborate, manage projects, and reach new productivity peaks. Kanban is a visual system for managing work as it flows through a process. Each column represents a step in the process, and each card represents a task. Cards can be moved between columns to show the progress of the task.",
 }
